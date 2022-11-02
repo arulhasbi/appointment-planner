8dbf8f56575ad86acd4002ece0c63b782cb78c2f
fakhrul hasbi <fakhrulhasbi@fakhruls-MacBook-Pro.local>
2022-11-02 21:17:52 +0800

    added appointment page form validation

--- src/Containers/Appointments/Appointments.js
+++ src/Containers/Appointments/Appointments.js
@@ -37,7 +37,11 @@ const Appointments = () => {
               onBlur={formik.handleBlur}
               onChange={formik.handleChange}
               value={formik.values.title}
+              className={formik.errors.title ? "error-box" : null}
             />
+            {formik.errors.title && formik.touched.title ? (
+              <p className="error-message">{formik.errors.title}</p>
+            ) : null}
           </div>
           <div className="form-control">
             <label htmlFor="contact">Contact</label>
@@ -46,23 +50,39 @@ const Appointments = () => {
               name="contact"
               id="contact"
               onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={formik.errors.contact ? "error-box" : null}
             >
               <option value="">Choose</option>
+              <option value="contact-1">Contact-1</option>
             </select>
+            {formik.errors.contact && formik.touched.contact ? (
+              <p className="error-message">{formik.errors.contact}</p>
+            ) : null}
           </div>
           <div className="form-control">
             <label htmlFor="date">Date</label>
             <DatePicker
-              className="full-width"
+              name="date"
               selected={date}
               onChange={(date) => setDate(date)}
               dateFormat="MMMM d, yyyy"
+              onBlur={formik.handleBlur}
+              className={
+                formik.errors.contact ? "full-width error-box" : "full-width "
+              }
             />
+            {formik.errors.date && formik.touched.date ? (
+              <p className="error-message">{formik.errors.date}</p>
+            ) : null}
           </div>
           <div className="form-control">
             <label htmlFor="time">Time</label>
             <DatePicker
-              className="full-width"
+              name="time"
+              className={
+                formik.errors.time ? "full-width error-box" : "full-width "
+              }
               selected={time}
               onChange={(date) => setTime(date)}
               showTimeSelect
@@ -70,7 +90,11 @@ const Appointments = () => {
               timeIntervals={15}
               timeCaption="Time"
               dateFormat="h:mm aa"
+              onBlur={formik.handleBlur}
             />
+            {formik.errors.time && formik.touched.time ? (
+              <p className="error-message">{formik.errors.time}</p>
+            ) : null}
           </div>
           <button type="submit">Submit</button>
         </form>
